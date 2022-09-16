c49afd8b38d93fbe81e02679779afcdce8eff61d
ZefangWang <91424340+ZefangWang@users.noreply.github.com>
2022-09-16 12:29:56 -0400

    Create Tweet api and tests

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -7,6 +7,10 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email']
 
+class UserSerializerForTweet(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
 
 class LoginSerializer(serializers.Serializer):
     username = serializers.CharField()
